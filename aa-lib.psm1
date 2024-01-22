fe2ba6fa771cff4f0f7bd690f9b7a8fe5cab3b01
tachyon-technical <157341889+tachyon-technical@users.noreply.github.com>
2024-01-21 22:21:20 -0700

    Add files via upload
    Initial upload

---
+++ aa-lib.psm1
@@ -0,0 +1,155 @@
+﻿
+
+# Parse INI file and transmute to a hash table
+function Get-IniContent ($FilePath) {
+
+  # Check if configuration file exists
+  # If it doesn't exist, print error and return $false
+  # If it does exist, print success and continue
+  $FilePathExists = [Boolean](Test-Path $FilePath -ErrorAction Stop)
+  if (!$FilePathExists) {
+    Write-Host -ForegroundColor Red "Unable to find configuration file"
+    return $false
+  } else {
+    Write-Host -ForegroundColor Green "Found configuration file"
+  }
+
+  # We now know the file exists
+  # Read file into hash table, account for [sections] and skipping ;comments
+  # Return the hash table
+  $ini = @{}
+  switch -regex -File $FilePath
+  {
+    “^\[(.+)\]” # Section
+    {
+      $section = $matches[1]
+      $ini[$section] = @{}
+    }
+    “^(;.*)$” # Comment
+    {
+      continue
+    }
+    “(.+?)\s*=(.*)” # Key
+    {
+      $name,$value = $matches[1..2]
+      $ini[$section][$name] = $value.trim('"')
+    }
+  }
+  if (!$ini) {
+    Write-Host -ForegroundColor Red "Failed to load configuration file"
+    exit
+  } else {
+    Write-Host -ForegroundColor Green "Loaded configuration file"
+    return $ini
+  }
+}
+
+# Confirm admin privileges
+function Get-AdminPrivilege () {
+  # Get current user and check if administrator
+  $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+  $IsAdmin = (New-Object Security.Principal.WindowsPrincipal $CurrentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+  if (!$IsAdmin) {
+    Write-Host -ForegroundColor Red "Script must be running with admin privileges"
+    return $false
+  } else {
+    Write-Host -ForegroundColor Green "Script running with admin privileges"
+    return $true
+  }
+  return $true
+}
+
+# Generate a random password for .PFX file
+function New-PfxPassword () {
+  Write-Host -ForegroundColor Green "Generating a random .PFX password"
+  Add-Type -AssemblyName System.Web
+  return [System.Web.Security.Membership]::GeneratePassword(24,2)
+}
+
+# Open the inbound port (80/TCP)
+function Set-InboundPort ($port) {
+
+  # Create firewall rule or fails
+  try {
+    New-NetFirewallRule -DisplayName "Allow TCP $port" `
+       -LocalPort $port `
+       -Action Allow `
+       -Profile Any `
+       -Protocol TCP `
+       -Direction Inbound `
+       -Enabled True `
+       -ErrorAction Stop |
+    Out-Null
+    Write-Host -ForegroundColor Green "Opened TCP $port inbound"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to open TCP $port inbound"
+    exit
+  }
+}
+
+# Close the inbound port (80/TCP)
+function Remove-InboundPort ($port) {
+  
+  # Remove firewall rule or fail
+  try {
+    Remove-NetFirewallRule -DisplayName "Allow TCP $port" -ErrorAction Stop
+    Write-Host -ForegroundColor Green "Closed TCP $port inbound"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to closed TCP $port inbound"
+    exit
+  }
+}
+
+# Restrict filesystem permissions to Administrators, SYSTEM, and OWNER
+function Set-RestrictedPermissions ($filename) {
+  Write-Host -ForegroundColor Green "Removing existing permissions and inheritance for $filename"
+
+  # Get existing ACL
+  try {
+    $acl = Get-Acl $filename -ErrorAction Stop
+    Write-Host -ForegroundColor Green "`t=> Collected current permissions"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to collect current permissions"
+    exit
+  }
+
+  # Remove inheritance
+  try {
+    $acl.SetAccessRuleProtection($true,$false)
+    Write-Host -ForegroundColor Green "`t=> Purged inherited permissions"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to purge inherited permission"
+    exit
+  }
+
+  # Define new ACEs
+  try {
+    $admin_full = New-Object System.Security.AccessControl.FileSystemAccessRule ("BUILTIN\Administrators","FullControl","Allow")
+    $owner_full = New-Object System.Security.AccessControl.FileSystemAccessRule ("CREATOR OWNER","FullControl","Allow")
+    $system_full = New-Object System.Security.AccessControl.FileSystemAccessRule ("NT AUTHORITY\SYSTEM","FullControl","Allow")
+    Write-Host -ForegroundColor Green "`t=> Created ACEs for Administrators, OWNER, and SYSTEM"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to created ACEs for Administrators, OWNER, and SYSTEM"
+    exit
+  }
+
+  # Apply ACEs to ACL 
+  try {
+    $acl.SetAccessRule($admin_full)
+    $acl.SetAccessRule($owner_full)
+    $acl.SetAccessRule($system_full)
+    Write-Host -ForegroundColor Green "`t=> Applied ACEs to ACL"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to apply ACEs to ACL"
+    exit
+  }
+
+  # Apply ACL to file
+  try {
+    Set-Acl $filename $acl -ErrorAction Stop
+    Write-Host -ForegroundColor Green "`t=> Applied ACL to $filename"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to apply ACL to $filename"
+  }
+}
---
+++ autoacme.ps1
@@ -0,0 +1,104 @@
+﻿###
+### SETUP: Define variables; confirm location, library, privileges, and config are all OK.
+###
+
+# Define variables
+$LibFile = "aa-lib.psm1"
+$ConfFile = "conf.ini"
+
+# Confirm we're in the script directory; otherwise exit
+try {
+  Set-Location -Path $PSScriptRoot -ErrorAction Stop
+  Write-Host -ForegroundColor Green "Set working directory to $PSScriptRoot"
+} catch {
+  Write-Host -ForegroundColor Red "Failed to set working directory to $PSScriptRoot"
+  exit
+}
+
+# Confirm library is present, attempt to import; otherwise exit
+if (Test-Path "$PSScriptRoot\$LibFile") {
+  Write-Host -ForegroundColor Green "Found $LibFile"
+  try {
+    Import-Module "$PSScriptRoot\$LibFile" -Scope Global -ErrorAction Stop -Force
+    Write-Host -ForegroundColor Green "Imported $LibFile"
+  } catch {
+    Write-Host -ForegroundColor Red "Failed to import $LibFile"
+    exit
+  }
+} else {
+  Write-Host -ForegroundColor Red "Failed to find $LibFile"
+  exit
+}
+
+# Confirm we're running as admin; otherwise exit
+if (!(Get-AdminPrivilege)) {
+  exit
+}
+
+# Load configuration into a hash table
+$conf = Get-IniContent ("$PSScriptRoot\$ConfFile")
+
+###
+### MAIN: Created PFX password, open ports, collect certificate, close ports, and save everything
+###
+
+# Generate a random password for the .pfx file
+$PfxPassword = New-PfxPassword
+
+# Open inbound port (ACME requires 80 currently)
+Set-InboundPort $conf["crt"]["listen_port"]
+
+# Print arguments
+Write-Host -ForegroundColor Green "ACME settings as follows:"
+Write-Host -ForegroundColor Green "`t=> ACME URL: " $conf["crt"]["server"]
+Write-Host -ForegroundColor Green "`t=> Domain: " $conf["crt"]["domain"]
+Write-Host -ForegroundColor Green "`t=> Contact: " $conf["crt"]["contact"]
+Write-Host -ForegroundColor Green "`t=> Account key: " $conf["crt"]["key_type"]
+Write-Host -ForegroundColor Green "`t=> Certificate key: " $conf["crt"]["key_type"]
+Write-Host -ForegroundColor Green "`t=> Listener port: " $conf["crt"]["listen_port"]
+Write-Host -ForegroundColor Green "`t=> Listener wait: " $conf["crt"]["listen_wait"]
+
+# Define arguments for ACME certificate request
+$mArgs = @{
+  DirectoryUrl = $conf["crt"]["server"]
+  Domain = $conf["crt"]["domain"]
+  Contact = $conf["crt"]["contact"]
+  AccountKeyLength = $conf["crt"]["key_type"]
+  CertKeyLength = $conf["crt"]["key_type"]
+  Name = $conf["crt"]["name"]
+  FriendlyName = $conf["crt"]["name"]
+  PfxPass = $PfxPassword
+}
+
+# Define arguments for ACME self-hosting plugin
+$pArgs = @{
+  WSHDelayAfterStart = $conf["crt"]["listen_wait"]
+  WSHPort = $conf["crt"]["listen_port"]
+}
+
+# Perform certificate request
+try {
+  $cert = New-PACertificate @mArgs -Plugin WebSelfHost -PluginArgs $pArgs -AcceptTOS -Force
+  Write-Host -ForegroundColor Green "Recieved certificate"
+} catch {
+  Write-Host -ForegroundColor Red "Failed to recieve certificate"
+  exit
+}
+
+# Close inbound port (or more accurately, remove the rule we made)
+Remove-InboundPort $conf["crt"]["listen_port"]
+
+# Copy .PFX file to script directory; timestamp name
+$pfxFile = Copy-Item -Path $cert.PfxFile -Destination $PSScriptRoot -Passthru
+Write-Host -ForegroundColor Green "Copied .PFX file to $PSScriptRoot"
+
+# Write .PFX password to a text file
+$PfxPassFile = "$(Get-Date -Format MM_dd_yy)" + "_" + $cert.AllSans + ".txt"
+$PfxPassword | Out-File -FilePath $PfxPassFile -Force
+Write-Host -ForegroundColor Green "Saved .PFX password in $PfxPassFile"
+
+# Restrict NTFS permissions to Adminstrators, SYSTEM, and OWNER
+Set-RestrictedPermissions ($PfxFile.FullName)
+
+# Restrict NTFS permissions to Adminstrators, SYSTEM, and OWNER
+Set-RestrictedPermissions ($PfxPassFile)
